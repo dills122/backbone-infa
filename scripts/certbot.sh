9129f24cff86b93896116eb9365bd3239305c4da
Dylan Steele <dylansteele57@gmail.com>
2022-02-17 22:58:45 -0500

    fixed dir in certbot script

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -7,7 +7,7 @@ ssl_email="dylansteele57@gmail.com"
 #TODO fix issue with nginx config/ cert install; cant have listen 80 and 443 in same server, certbot doesnt remove the 80
 sudo rm -rf /etc/nginx/conf.d/* && sudo rm -rf /etc/nginx/sites-enabled/default
 # Copy Nginx config files
-sudo cp ~/app-src/.docker/nginx/conf.d/prod/* /etc/nginx/sites-enabled/
+sudo cp ~/backbone-src/.docker/nginx/conf.d/prod/* /etc/nginx/sites-enabled/
 
 sudo nginx -t
 
