a92dd3bee534cc8d44561d99f1bb5ea6ff9bd8f4
Dylan Steele <dylansteele57@gmail.com>
2023-12-14 20:55:15 -0500

    fixed bugs with terraform & umami setup

--- scripts/umami-setup.sh
+++ scripts/umami-setup.sh
@@ -4,8 +4,11 @@ git clone https://github.com/mikecao/umami.git
 
 pushd umami
 
-npm i
-npm run build
+sudo npm i -g yarn
+yarn install
+# yarn build
+
+echo "DATABASE_URL=postgresql://main:Ba21tedao23094!@localhost:5432/mydb" >.env
 
 docker pull ghcr.io/mikecao/umami:postgresql-latest
 docker-compose up -d
