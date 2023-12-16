8067a6ddda2c09dddd374e914a440141e4ccc67c
Dylan Steele <dylansteele57@gmail.com>
2023-12-15 21:31:12 -0500

    Deployment script for `dsteele.dev` (#7)
    * added script for personal site setup
    
    * initial fixes from testing
    
    * initial site setup & deployment
    
    * minor fixes
    
    * added git pull
    
    * try changes to confs
    
    * another one
    
    * removed pushd/popd
    
    * finished fixes & got deployed
    
    * removed pushd/popd

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-domain="dsteele.dev"
-umami_sub_domain="umami"
+DOMAINDEFAULT="dsteele.dev"
+domain="${1:-$DOMAINDEFAULT}"
+sub_domain="$2"
 ssl_email="dylansteele57@gmail.com"
 
-# Need to update the naming to the url of site ex. umami.dsteele.dev
-sudo rm -rf /etc/nginx/conf.d/* && sudo rm -rf /etc/nginx/sites-available/*
-# Copy Nginx config files
-sudo cp ~/backbone-src/.docker/nginx/conf.d/prod/* /etc/nginx/sites-available/
+generateSSL() {
+    local full_domain=$1
+    # Copy Nginx config files
+    sudo cp ~/backbone-src/.docker/nginx/conf.d/prod/${full_domain}.conf /etc/nginx/sites-available/
 
-sudo nginx -t
+    sudo nginx -t
 
-sudo certbot --nginx --non-interactive --agree-tos --redirect -d ${umami_sub_domain}.${domain} -d www.${umami_sub_domain}.${domain} -m ${ssl_email}
+    sudo certbot --nginx --non-interactive --agree-tos --redirect -d ${full_domain} -d www.${full_domain} -m ${ssl_email}
+}
+
+if [ -n "$sub_domain" ]; then
+    generateSSL "$sub_domain.$domain"
+else
+    generateSSL "$domain"
+fi
--- scripts/umami-setup.sh
+++ scripts/umami-setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+/usr/bin/bash certbot.sh dsteele.dev umami
+
 git clone https://github.com/mikecao/umami.git
 
-pushd umami
+cd ./umami
 
 sudo npm i -g yarn
 yarn install
@@ -11,6 +13,6 @@ yarn install
 echo "DATABASE_URL=postgresql://main:Ba21tedao23094!@localhost:5432/mydb" >.env
 
 docker pull ghcr.io/mikecao/umami:postgresql-latest
-docker-compose up -d
+docker compose up -d
 
-popd
+cd ..
